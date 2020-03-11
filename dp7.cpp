df0cb887c6f2823c4b985ed935c2f0d6077ff514
Ritwik Baranwal <45766294+Vortez2471@users.noreply.github.com>
2020-03-11 20:10:33 +0530

    Add files via upload

---
+++ dp7.cpp
@@ -0,0 +1,40 @@
+//Edit distance.....sunday to saturday conversion
+//insertion or deletion or replacement-> costs 1 for each
+#include<iostream>
+#include<bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+int editdist(char inp[100],char out[100])
+{
+	int dp[101][101]={};
+
+	int m=strlen(inp);
+	int n=strlen(out);
+
+	for(int i=1;i<n;i++) dp[0][i]=dp[0][i-1] +1;
+	for(int j=1;j<=m;j++) dp[j][0]=dp[j-1][0]+1;
+
+    for(int i=1;i<=m;i++)
+    {
+    	for(int j=1;j<=n;j++)
+    	{
+    		int q1=dp[i-1][j-1];
+    		int q2=dp[i-1][j];
+    		int q3=dp[i][j-1];
+    		dp[i][j]=min(q1,min(q2,q3))+(inp[i-1]!=out[j-1]);
+    	}
+    	return dp[m][n];
+
+    }
+}
+
+int main()
+{
+  char inp[100],out[100];
+  cin>>inp>>out;
+
+  int ans=editdist(inp,out);
+
+  cout<<ans<<endl;
+}
